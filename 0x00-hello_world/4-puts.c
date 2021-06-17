84e3bfcff230b4a8e733ae3d8835dfdfc77fd0dd
juanescorreap <juanestebancope@outlook.com>
2021-06-17 15:28:50 -0700

    print to standard output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 /*
- *Print to the standard output
+ * prints holberton proyect 0x00 4.
  */
 
 #include <stdio.h>
 
-int main(void)
+int
+main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
