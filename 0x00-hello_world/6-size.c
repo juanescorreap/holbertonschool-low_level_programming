b51f091cb468f08bbc3bf804db33a772a30c2610
juanescorreap <juanestebancope@outlook.com>
2021-06-17 16:08:45 -0700

    Printf to display the size of various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+/*
+ * use printf to display the size fo various data types
+ */
+
+#include <stdio.h>
+
+int
+main(void)
+{
+printf("Size of a char: %d\n", sizeof(char));
+printf("Size of an int: %d\n", sizeof(int));
+printf("Size of a long int: %d\n", sizeof(long int));
+printf("Size of a long long int: %d\n", sizeof(long long int));
+printf("Size of a float: %d\n", sizeof(float));
+return (0);
+}
