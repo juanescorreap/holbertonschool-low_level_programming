02ef1cd5dfda29ff3332b852a65d264ba11078c4
juanescorreap <juanestebancope@outlook.com>
2021-06-17 15:05:48 -0700

    Print to the standard output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-  /*Print to the standard output*/
-  puts (\"Programming is like building a multilingual puzzle\n);
-  return(0);
-    }
+/*Print to the standard output*/
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
