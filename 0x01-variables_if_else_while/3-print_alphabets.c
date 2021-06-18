65e607ce13c9dcd04ff092062ea1364730c2f23c
juanescorreap <juanestebancope@outlook.com>
2021-06-18 15:49:40 -0700

    Print the alphabet both in lowe and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Prints the alphabet
+* Return: always returns to 0
+*/
+int
+main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar (ch);
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+putchar (ch);
+{
+putchar (012);
+}
+return (0);
+}
