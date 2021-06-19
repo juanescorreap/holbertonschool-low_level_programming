01d748f25a6f91bf56cd8595128b3999e400b7dc
juanescorreap <juanestebancope@outlook.com>
2021-06-18 19:09:52 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet
+* Return: always returns to 0
+*/
+int main(void)
+{
+int ch = 48;
+while (ch <= 57)
+{
+putchar (ch);
+ch++;
+}
+putchar (10);
+return (0);
+}
