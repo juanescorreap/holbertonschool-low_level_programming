87976c8f1895928233105db51df57bee91b8f8dc
juanescorreap <juanestebancope@outlook.com>
2021-06-22 12:52:09 -0700

    print holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,15 +2,13 @@
 /**
 * main - Prints "Holberton\n" using _putchar
 * Return: Returns to zero
-*
 */
 int main(void)
 {
-int c;
-char ph[10] = "Holberton\n";
-for (c = 0; c <= 10; c++)
+char *ph = "Holberton\n";
+while (*ph)
 {
-_putchar(ph[c]);
+_putchar(*ph++);
 }
 return (0);
 }
