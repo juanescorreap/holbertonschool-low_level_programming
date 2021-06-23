cd0898fee80aa89444ce549ebc671485abdc5345
juanescorreap <juanestebancope@outlook.com>
2021-06-22 17:52:20 -0700

    print the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,17 @@
 */
 void print_alphabet_x10(void)
 {
-char ch = 97;
-int n = ch / 122;
-while (n <= 10)
-{
-while (ch <= 122)
-{
-_putchar(ch);
-ch++;
-}
-_putchar(10);
-}
+	char ch = 97;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		while (ch <= 122)
+		{
+			_putchar(ch);
+			ch++;
+		}
+		ch = 97;
+		_putchar(10);
+	}
 }
