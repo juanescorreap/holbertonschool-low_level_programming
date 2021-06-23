a1dd3cdfdf2518244d70e14adb84d187de2e8456
juanescorreap <juanestebancope@outlook.com>
2021-06-23 14:08:18 -0700

    print mmultiplication tables with special format

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+* times_table - Prints every the nine times table
+* Return: returns to zero
+*/
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+	
+	for (y = 0; y <= 9; y++)
+		{
+			for (x = 0; x <= 9; x++)
+			{
+				z= (x * y);
+				if (z / 10 ==0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z % 10) + '0');
+					if(x < 9)
+					{	
+					_putchar(',');
+					}
+				 }
+				else
+				{
+			 _putchar(' ');
+			_putchar((z / 10) + '0');
+			_putchar((z % 10) + '0');
+			if(x < 9)
+			{
+			_putchar(',');
+			}
+				}
+
+			}
+			_putchar(10);
+		  }
+}
