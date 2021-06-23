a139537973461ec1aebd8a01475ba207ee603a97
juanescorreap <juanestebancope@outlook.com>
2021-06-23 15:23:51 -0700

    add two integers and return the value

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
 * add - adds two integers and returns the result
 * Return: returns to zero
-* @x: First integer in the operation 
+* @x: First integer in the operation
 * @y: second integer in the operation
 */
 int add(int x, int y)
