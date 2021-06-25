2d4420bd59b9e8fadd8a0b349b3f567e47de192d
juanescorreap <juanestebancope@outlook.com>
2021-06-25 10:26:49 -0700

    funtion to  multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - multiples two integers
+ * Return: returns the product og the multiplication
+ * @a: first integer to be multiplied
+ * @b: second integer to be multiplied
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
 #define HOLBERTON_H
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
