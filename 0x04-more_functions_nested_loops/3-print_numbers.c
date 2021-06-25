82ae21a189c2ff8596a418722350e8e68f8f64ba
juanescorreap <juanestebancope@outlook.com>
2021-06-25 11:10:41 -0700

    print numbers form 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,9 @@
 /**
  * print_numbers - prints number from 0 to 9
  * Return: zero
- * @a:variable that takes the value to be printed
  */
 void print_numbers(void)
-
 {
-	int a;
-
 	for (a = 48; a >= 48 && a <= 57; a++)
 	{
 		_putchar(a);
