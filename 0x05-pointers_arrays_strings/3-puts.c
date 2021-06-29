f66460cbd7fe3ff3b7bd84eb47709b58cd636fda
juanescorreap <juanestebancope@outlook.com>
2021-06-29 23:42:34 +0000

    Prints a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _puts - Prints a string followed by a new line
+ * Return: void
+ * @str: String to be printed
+ */
+void _puts(char *str)
+{
+	for (; *str; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar(10);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
 #define HOLBERTON_H
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(int ch);
