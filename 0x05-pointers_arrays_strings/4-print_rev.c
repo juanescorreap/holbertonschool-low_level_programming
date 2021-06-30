5d033f957cd40fc5a32e092e11755889f850cb09
juanescorreap <juanestebancope@outlook.com>
2021-06-30 02:10:32 +0000

    Prints a string in reverse followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * Return: void
+ * @s: String to be printed
+ */
+void print_rev(char *s)
+{
+	int l;
+	int g;
+	int h;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+	}
+	h = l;
+	for (g = h - 1; g >= 0; g--)
+	{
+	_putchar(s[g]);
+	}
+	_putchar(10);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -4,3 +4,4 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(int ch);
+void print_rev(char *s);
