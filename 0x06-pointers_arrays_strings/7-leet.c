0cf9274247aa20b62084bbf2f359e8dd9e106efb
juanescorreap <juanestebancope@outlook.com>
2021-07-03 03:50:21 +0000

    Function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * *leet - Function that encodes a string into 1337.
+ * Return: String of type char
+ * @s: String to be encoded
+ * @x: Alphabet array
+ * @y: Numbers array
+ */
+char *leet(char *s)
+{
+	char x[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char y[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int i;
+	int j;
+
+	for (i = 0; s[i] !=0; i++)
+	{
+		for (j = 0; j < 10 ; j++)
+		{
+			if (s[i] == x[j])
+			{
+				s[i] = y [j];
+			}
+		}
+	}
+	return (s);
+}
