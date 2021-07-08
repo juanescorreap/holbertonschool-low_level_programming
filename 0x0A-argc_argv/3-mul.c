50091eb3c56f646afd89528418e2cfc5e51d8291
juanescorreap <juanestebancope@outlook.com>
2021-07-08 21:03:30 +0000

    Program that prints the result of a multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program that prints the result of a multiplication
+ * Return:Integer, 0 if succesful
+ * @argc: Number of arguments passed to the function
+ * @argv: array of strings cointaining the arguments
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (49);
+	}
+	else
+	{
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+	}
+}
