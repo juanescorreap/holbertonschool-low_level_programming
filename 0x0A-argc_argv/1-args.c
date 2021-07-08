0e9146f971b7d3f9e1f8e88f6823a40ccfc009af
juanescorreap <juanestebancope@outlook.com>
2021-07-08 19:14:52 +0000

    Program that prints its name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Program that prints its name
+ * Return: Integer, 0 if succesful
+ *@argc: Number of arguments passed to the function
+ * @argv: array of strings cointaining the arguments
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
