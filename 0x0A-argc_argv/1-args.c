da08a48d77f3e4694f2963d9335fbfe27932d4c6
juanescorreap <juanestebancope@outlook.com>
2021-07-08 19:43:45 +0000

    Program that printst the number of arguments the function receives

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - Program that prints its name
+ * main - Program that printst the number of arguments the function receives
  * Return: Integer, 0 if succesful
  * @argc: Number of arguments passed to the function
  * @argv: array of strings cointaining the arguments
  */
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
