c2d595e84c611cdf09b617d8449106fa78737be4
juanescorreap <juanestebancope@outlook.com>
2021-07-19 17:41:32 -0500

    Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - Short description
+ * @name: Pointer that holds the address to "name"
+ * @age: Float that holds the vaule "age"
+ * @owner: Pointer that holds the address to "owner"
+ * Description: Structure that holds multiple values
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
