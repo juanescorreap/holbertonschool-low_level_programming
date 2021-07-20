7600ffb44caecbd13bab1f1c0b0ca73fc283d2c0
juanescorreap <juanestebancope@outlook.com>
2021-07-19 22:37:52 -0500

    Function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * *new_dog - Function that creates a new dog.
+ * @name: Pointer that holds the addres to "name"
+ * @age: Variable of type float that holds "age"
+ * @owner: Pointer that hold the address to "owner"
+ * Description: Structure that holds multiple values
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *newpuppy;
+    int x, c;
+    char *copianame, *copiaowner;
+
+    newpuppy = malloc(sizeof(dog_t));
+    if (newpuppy == NULL)
+        return (NULL);
+    for (x = 0; name[x] != '\0'; x++)
+    {
+    }
+    for (c = 0; owner[c] != '\0'; c++)
+    {
+    }
+    copianame = malloc(sizeof(char *) * x + 1);
+    if (copianame == NULL)
+    {
+        free(newpuppy);
+        return (NULL);
+    }
+    copiaowner = malloc(sizeof(char *) * c + 1);
+    if (copiaowner == NULL)
+    {
+        free(copianame);
+        free(newpuppy);
+        return (NULL);
+    }
+    for (x = 0; name[x] != '\0'; x++)
+    {
+        copianame[x] = name[x];
+    }
+    copianame[x] = '\0';
+    for (c = 0; owner[c] != '\0'; c++)
+    {
+        copiaowner[c] = owner[c];
+    }
+    copiaowner[c] = '\0';
+    newpuppy->name = copianame;
+    newpuppy->age = age;
+    newpuppy->owner = copiaowner;
+    return (newpuppy);
+}
