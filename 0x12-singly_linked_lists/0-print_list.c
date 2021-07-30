30fd31b9893079a6db551168e80d625df7838121
juanescorreap <juanestebancope@outlook.com>
2021-07-30 17:26:04 -0500

    task 1 and lists.h library

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - Function that prints all the elements of a list_t list.
+ * @h: pointer to a list_t element
+ * Return: A size_t value for list_t
+ */
+size_t print_list(const list_t *h)
+{
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+
+			printf("[%d] %s", h->len, h->str);
+		}
+		h = h->next;
+		printf("\n");
+	}
+	return (sizeof(list_t));
+}
