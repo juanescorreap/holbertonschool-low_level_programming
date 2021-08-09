cd5895be2c0230ab3d1a09af5c839cb93f356673
juanescorreap <juanestebancope@outlook.com>
2021-08-09 17:19:49 -0500

    Function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,17 +21,34 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (fd == -1)
+	if (text_content == NULL)
 	{
-		return (-1);
+		fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		fd2 = write(fd, text_content, 0);
+		if (fd2 == -1)
+		{
+			return (-1);
+		}
+		return (1);
 	}
-	fd2 = write(fd, text_content, strlen(text_content));
-	if (fd2 == -1)
+	else
 	{
-		return (-1);
+		fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		fd2 = write(fd, text_content, strlen(text_content));
+		if (fd2 == -1)
+		{
+			return (-1);
+		}
+		return (1);
 	}
 	close(fd);
 
-	return (1);
 }
