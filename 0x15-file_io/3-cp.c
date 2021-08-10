29517ce8c67443b30cda3d0767c182c8d4fb4bb5
juanescorreap <juanestebancope@outlook.com>
2021-08-09 21:59:27 -0500

    Program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,22 +6,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "main.h"
-/**
- * main - Program that copies the content of a file to another file.
- * @argc:Argument counter
- * @argv:Argument vectors
- * Return: int.
- */
-int main(int argc, char *argv[])
+
+void file_copy(char *argv[])
 {
 	int fd = 0, fd2 = 0, fd3 = 1, fd4 = 0, fd5 = 0, fd6 = 0;
 	char buffer[1024];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
@@ -43,7 +33,7 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 		fd4 = write(fd2, buffer, fd3);
-		if(fd4 == -1)
+		if (fd4 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -59,5 +49,20 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd6);
 		exit(100);
 	}
+}
+/**
+ * main - Program that copies the content of a file to another file.
+ * @argc:Argument counter
+ * @argv:Argument vectors
+ * Return: int.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_copy((char **)argv);
 	return (0);
 }
