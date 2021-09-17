b0e37f501c222a6373c8a20f5ca925582080ee14
juanescorreap <juanestebancope@outlook.com>
2021-09-17 10:09:54 -0500

    edge case of null *head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,14 +21,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		new_node->n = n;
-		new_node->next = *head;
+		new_node->next = NULL;
 		new_node->prev = NULL;
 		*head = new_node;
 		return (*head);
 	}
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *head;
-	*head = new_node;
-	return (*head);
+	else
+	{
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
 }
