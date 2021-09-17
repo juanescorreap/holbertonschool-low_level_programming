dabba716a3bb3c2824e3996dca592bb20fa66d8b
juanescorreap <juanestebancope@outlook.com>
2021-09-17 10:11:50 -0500

    edge case of null *head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -24,7 +24,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new_node->next = NULL;
 		new_node->prev = NULL;
 		*head = new_node;
-		return (*head);
 	}
 	else
 	{
@@ -32,6 +31,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new_node->prev = NULL;
 		new_node->next = *head;
 		*head = new_node;
-		return (*head);
 	}
+	return (*head);
 }
