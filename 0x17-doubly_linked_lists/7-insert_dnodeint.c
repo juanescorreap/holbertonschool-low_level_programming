5436bc27acb139f3100388b629d402f5cdc07894
juanescorreap <juanestebancope@outlook.com>
2021-09-17 16:43:09 -0500

    call function form task 2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,6 +25,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
 	while (current)
 	{
 		if (count == idx)
@@ -40,6 +44,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count = count + 1;
 	}
 	return (NULL);
-
-
 }
