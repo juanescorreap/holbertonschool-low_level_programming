f6c6d359d39346efe44a6bd6cff3f1109650fcb1
John Doe <juanestebancope@outlook.com>
2021-10-09 17:27:52 -0500

    changing file of function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,26 @@
 #include "hash_tables.h"
 /**
+ * hs_allocate - Function that adds an
+ * element to the hash table.
+ * Return: 1 if it succeeded, 0 otherwise
+ * @key: key to be added
+ * @value: value that goes with the key
+ *
+ */
+hash_node_t *hs_allocate(const char *key, const char *value)
+{
+	hash_node_t *entry = NULL;
+
+	entry = malloc(sizeof(hash_node_t*));
+
+	entry->key = (char*)key;
+	entry->value = (char*)value;
+	entry->next = NULL;
+
+	return(entry);
+}
+
+/**
  * hash_table_set - Function that adds an
  * element to the hash table.
  * Return: 1 if it succeeded, 0 otherwise
