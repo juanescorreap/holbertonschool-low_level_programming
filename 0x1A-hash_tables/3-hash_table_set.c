ba831b0670e8177bb6028df792df8132e3a76dd6
John Doe <juanestebancope@outlook.com>
2021-10-10 14:38:09 -0500

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,26 +1,38 @@
 #include "hash_tables.h"
-
 /**
- * hs_allocate - Function that adds an
- * element to the hash table.
+ * hash_table_set - Function that creates a
+ * node 
  * Return: 1 if it succeeded, 0 otherwise
  * @key: key to be added
  * @value: value that goes with the key
+ * @ht: Pointer to hash table
  *
  */
-hash_node_t *hs_allocate(const char *key, const char *value)
+hash_node_t *create_node(char *cp_key, char *cp_value)
 {
 	hash_node_t *entry = NULL;
 
 	entry = malloc(sizeof(hash_node_t *));
-
-	entry->key = (char *)key;
-	entry->value = (char *)value;
-	entry->next = NULL;
+	if (entry == NULL)
+	{
+		return (0);
+	}
+	entry->key = cp_key;
+	if (entry->key == NULL)
+	{
+		free(entry);
+		return (0);
+	}
+	entry->value = cp_value;
+	if (entry->value == NULL)
+	{
+		free(entry->key);
+		free(entry);
+		return (0);
+	}
 
 	return (entry);
 }
-
 /**
  * hash_table_set - Function that adds an
  * element to the hash table.
@@ -32,9 +44,11 @@ hash_node_t *hs_allocate(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
 	hash_node_t *entry = NULL;
 	hash_node_t *tmp = NULL;
+	unsigned long int index = 0;
+	char *cp_value = NULL;
+	char *cp_key = NULL;
 
 	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 	{
@@ -42,27 +56,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	index = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[index];
-
+	cp_value = strdup(value);
+	cp_key = strdup(key);
+	if (cp_value == NULL)
+	{
+		return (0);
+	}
 	while (tmp != NULL)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
 			free(tmp->value);
-			tmp->value = strdup(value);
+			tmp->value = cp_value;
 			return (1);
 		}
 		tmp = tmp->next;
 	}
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = hs_allocate(key, strdup(value));
-		return (1);
-	}
-	else
-	{
-		entry->next = ht->array[index];
-		ht->array[index] = hs_allocate(key, strdup(value));
-		return (1);
-	}
-	return (0);
+	entry = create_node(cp_key, cp_value);
+	entry->next = ht->array[index];
+	ht->array[index] = entry;
+	return (1);
 }
