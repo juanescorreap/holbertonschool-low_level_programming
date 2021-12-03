3a087e49dc88ecef7aa4167fccec885d0d861a9d
John Doe <juanestebancope@outlook.com>
2021-12-03 11:12:32 -0500

    enumerate() method instead of range(len())

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,21 +4,22 @@ Function that returns the perimeter
 of the island described
 """
 
+
 def island_perimeter(grid):
     """
     Function that returns the perimeter
     of the island described
     """
     perimeter = 0
-    for j, row in range(len(grid)):
-        for k, cell in range(len(row)):
+    for j, row in enumerate(grid):
+        for k, cell in enumerate(row):
             if cell == 1:
                 if j == 0 or grid[j - 1][k] == 0:
                     perimeter = perimeter + 1
-                if j == len(grid) -1 or grid[j + 1][k] == 0:
+                if j == len(grid) - 1 or grid[j + 1][k] == 0:
                     perimeter = perimeter + 1
                 if k == 0 or grid[j][k - 1] == 0:
                     perimeter = perimeter + 1
                 if k == len(row) - 1 or grid[j][k + 1] == 0:
-                    perimeter = perimeter +1
+                    perimeter = perimeter + 1
     return perimeter
