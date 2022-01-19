90206455df14718c1ce8171b7638b4152f8ea4de
John Doe <juanestebancope@outlook.com>
2022-01-18 22:01:18 -0500

    variable declaration

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,13 +9,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	int i = 0;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
-	for (i = 0; i < (int)size; i++)
+	for (; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
